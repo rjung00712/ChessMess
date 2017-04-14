83e6c499de333dc56d446ee33d344a8b0c5547d5
rjung00712 <Richard@b98-aruba-authenticated-10-110-136-250.wlan.cpp.edu>
2017-04-14 11:47:50 -0700

    almost there

--- app/src/main/java/techiiesio/com/chessmess/ChessActivity.java
+++ app/src/main/java/techiiesio/com/chessmess/ChessActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.GridView;
 
+/**
+ * Created by Richard on 4/10/17.
+ */
+
 public class ChessActivity extends AppCompatActivity {
 
     static int[] images = {    // chess background images per each square of the chess board
@@ -30,14 +34,8 @@ public class ChessActivity extends AppCompatActivity {
             R.drawable.black_king, R.drawable.black_bishop, R.drawable.black_knight, R.drawable.black_rook,
             R.drawable.black_pawn, R.drawable.black_pawn, R.drawable.black_pawn, R.drawable.black_pawn,
             R.drawable.black_pawn, R.drawable.black_pawn, R.drawable.black_pawn, R.drawable.black_pawn,
-            R.drawable.tan_square, R.drawable.brown_square, R.drawable.tan_square, R.drawable.brown_square,
-            R.drawable.tan_square, R.drawable.brown_square, R.drawable.tan_square, R.drawable.brown_square,
-            R.drawable.brown_square, R.drawable.tan_square, R.drawable.brown_square, R.drawable.tan_square,
-            R.drawable.brown_square, R.drawable.tan_square, R.drawable.brown_square, R.drawable.tan_square,
-            R.drawable.tan_square, R.drawable.brown_square, R.drawable.tan_square, R.drawable.brown_square,
-            R.drawable.tan_square, R.drawable.brown_square, R.drawable.tan_square, R.drawable.brown_square,
-            R.drawable.brown_square, R.drawable.tan_square, R.drawable.brown_square, R.drawable.tan_square,
-            R.drawable.brown_square, R.drawable.tan_square, R.drawable.brown_square, R.drawable.tan_square,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
             R.drawable.white_pawn, R.drawable.white_pawn, R.drawable.white_pawn, R.drawable.white_pawn,
             R.drawable.white_pawn, R.drawable.white_pawn, R.drawable.white_pawn, R.drawable.white_pawn,
             R.drawable.white_rook, R.drawable.white_knight, R.drawable.white_bishop, R.drawable.white_queen,
@@ -46,15 +44,23 @@ public class ChessActivity extends AppCompatActivity {
 
     GridView gridView;  // gridview object that references the grid view container
 
+    private Board board;
+    private boolean hasOne;
+    private int position1;
+    private int position2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chess);
 
+        board = new Board();
+
         gridView = (GridView) findViewById(R.id.gridView);
 
         gridView.setStretchMode(GridView.STRETCH_COLUMN_WIDTH);
-        GridAdapter gridAdapter = new GridAdapter(this, images, pieces);
+        GridAdapter gridAdapter = new GridAdapter(this, images, pieces, this);
         gridView.setAdapter(gridAdapter);
     }
 
@@ -62,11 +68,122 @@ public class ChessActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
+//
+
         initializeBoard();
     }
 
     protected void initializeBoard() {
 
+    }
+
+    public Board getBoard() {return board;}
+
+    public void setBoard(Board b) {board = b;}
+
+    public void setHasOne(boolean h) {hasOne = h;}
+
+    public void setPosition1(int p) {position1 = p;}
+
+    public void setPosition2(int p) {position2 = p;}
+
+    public int getPosition1() {return position1;}
+
+    public int getPosition2() {return position2;}
+
+    public boolean isHasOne() {return hasOne;}
+
+    public void move()
+    {
+        int startX = getX(position1);
+        int startY = getY(position1);
+        int endX = getX(position2);
+        int endY = getY(position2);
+        boolean moved = board.makeMove(startX, startY, endX, endY);
+//        if(moved) {
+//            setPieces(board.getBoard());
+//            flipBoard();
+//        }
+        setPieces(board.getBoard());
+        GridAdapter gridAdapter = new GridAdapter(this, images, pieces, this);
+        gridView.setAdapter(gridAdapter);
+    }
+
+    public int getX(int position)
+    {
+        return position % 8;
+    }
+
+    public int getY(int position)
+    {
+        return position / 8;
+    }
+
+    public void setPieces(Piece[][] p)
+    {
+        for(int i = 0; i < 8; i++)
+            for(int j = 0; j < 8; j++)
+            {
+                if(p[i][j] != null)
+                {
+                    if(p[i][j] instanceof Pawn)
+                    {
+                        if(p[i][j].getColor() == 'W')
+                            pieces[(8 * i) + j] = R.drawable.white_pawn;
+                        else
+                            pieces[(8 * i) + j] = R.drawable.black_pawn;
+                    }
+                    else if(p[i][j] instanceof Rook)
+                    {
+                        if(p[i][j].getColor() == 'W')
+                            pieces[(8 * i) + j] = R.drawable.white_rook;
+                        else
+                            pieces[(8 * i) + j] = R.drawable.black_rook;
+                    }
+                    else if(p[i][j] instanceof Knight)
+                    {
+                        if (p[i][j].getColor() == 'W')
+                            pieces[(8 * i) + j] = R.drawable.white_knight;
+                        else
+                            pieces[(8 * i) + j] = R.drawable.black_knight;
+                    }
+                    else if(p[i][j] instanceof Bishop)
+                    {
+                        if(p[i][j].getColor() == 'W')
+                            pieces[(8 * i) + j] = R.drawable.white_bishop;
+                        else
+                            pieces[(8 * i) + j] = R.drawable.black_bishop;
+                    }
+                    else if(p[i][j] instanceof Queen)
+                    {
+                        if(p[i][j].getColor() == 'W')
+                            pieces[(8 * i) + j] = R.drawable.white_queen;
+                        else
+                            pieces[(8 * i) + j] = R.drawable.black_queen;
+                    }
+                    else
+                    {
+                        if(p[i][j].getColor() == 'W')
+                            pieces[(8 * i) + j] = R.drawable.white_king;
+                        else
+                            pieces[(8 * i) + j] = R.drawable.black_king;
+                    }
+                }
+                else
+                    pieces[(8 * i) + j] = -1;
+            }
+
+    }
+
+    public void flipBoard()
+    {
+        int[] temp = new int[pieces.length];
+        int x = pieces.length - 1;
+        for(int i = 0; i < pieces.length; i++, x--)
+            temp[x] = pieces[i];
+        pieces = temp;
+        GridAdapter gridAdapter = new GridAdapter(this, images, pieces, this);
+        gridView.setAdapter(gridAdapter);
     }
 
 }
--- app/src/main/java/techiiesio/com/chessmess/GridAdapter.java
+++ app/src/main/java/techiiesio/com/chessmess/GridAdapter.java
@@ -1,13 +1,14 @@
 package techiiesio.com.chessmess;
 
-import android.content.ClipData;
 import android.content.Context;
 import android.util.DisplayMetrics;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 /**
@@ -17,16 +18,20 @@ import android.widget.ImageView;
 public class GridAdapter extends BaseAdapter {
 
     Context context;
-    String[] values;
     int[] images;
     int[] pieces;
     View view;
     LayoutInflater layoutInflater;
+    boolean containsDragable;
+    ChessActivity chessActivity;
 
-    public GridAdapter(Context context, int[] images, int[] pieces) {
+//    int position;
+
+    public GridAdapter(Context context, int[] images, int[] pieces, ChessActivity chessActivity) {
         this.context = context;
         this.images = images;
         this.pieces = pieces;
+        this.chessActivity = chessActivity;
     }
 
     @Override
@@ -45,11 +50,14 @@ public class GridAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         if (convertView == null) {
             view = new View(context);
             view = layoutInflater.inflate(R.layout.activity_square, null);
+
+
+//            this.position = position;
 
             // get dimensions of width and height respective of screen resolution
             DisplayMetrics display = context.getResources().getDisplayMetrics();
@@ -60,31 +68,47 @@ public class GridAdapter extends BaseAdapter {
 //
 //        ImageView pieceImage = new ImageView(context);
 
-            ImageView squareImage = (ImageView) view.findViewById(R.id.square_background);
+            final ImageView squareImage = (ImageView) view.findViewById(R.id.square_background);
 
 //            squareImage.setLayoutParams(new FrameLayout.LayoutParams(squareHeight, squareWidth));
 
             squareImage.setImageResource(images[position]);
-            ImageView pieceImage = (ImageView) view.findViewById(R.id.piece);
-
+            final ImageView pieceImage = (ImageView) view.findViewById(R.id.piece);
 
 //        squareImage.setBackgroundResource(images[position]);
 
+//            pieceImage.setImageResource(pieces[position]);
 
+        if(pieces[position] != -1) {
             pieceImage.setImageResource(pieces[position]);
-
-
-//        if(pieces[position] != -1) {
-//            squareImage.setImageResource(pieces[position]);
-//        }
-
-
-            pieceImage.setOnTouchListener(new MyTouchListener());
         }
 
-        return view;
+            squareImage.setOnDragListener(new MyDragListener());
+
+//            pieceImage.setOnTouchListener(new MyTouchListener());
+
+            pieceImage.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                        if (chessActivity.isHasOne()) {
+                            chessActivity.setPosition2(position);
+                            chessActivity.move();
+                            chessActivity.setHasOne(false);
+                        } else {
+                            chessActivity.setPosition1(position);
+                            chessActivity.setHasOne(true);
+                            squareImage.setBackgroundResource(R.drawable.tan_square);
+                        }
+                    }
+                    return true;
+                }
+            });
+
     }
 
+        return view;
+}
 
 //    @Override
 //    public View getView(int position, View convertView, ViewGroup parent) {
@@ -120,20 +144,152 @@ public class GridAdapter extends BaseAdapter {
 //        return view;
 //    }
 
-    // This defines your touch listener
-    private final class MyTouchListener implements View.OnTouchListener {
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                ClipData data = ClipData.newPlainText("", "");
-                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(
-                        view);
-                view.startDrag(data, shadowBuilder, view, 0);
-                view.setVisibility(View.INVISIBLE);
-                return true;
-            } else {
-                return false;
+//    // This defines your touch listener
+//    private final class MyTouchListener implements View.OnTouchListener {
+//        public boolean onTouch(View view, MotionEvent motionEvent) {
+//            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+//                ClipData data = ClipData.newPlainText("", "");
+//                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(
+//                        view);
+//
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//                    view.startDragAndDrop(data, shadowBuilder, view, 0);
+//                } else {
+//                    view.startDrag(data, shadowBuilder, view, 0);
+//                }
+//                view.setVisibility(View.INVISIBLE);
+//                return true;
+//            } else {
+//                return false;
+//            }
+//        }
+//    }
+
+
+
+
+
+    class MyDragListener implements View.OnDragListener {
+//        Drawable enterShape = getResources().getDrawable(
+//                R.drawable.shape_droptarget);
+//        Drawable normalShape = getResources().getDrawable(R.drawable.shape);
+
+        FrameLayout frameLayout;
+        ImageView imageView;
+
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int action = event.getAction();
+
+            View dragView = (View) event.getLocalState();
+
+
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    // do nothing
+                    break;
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    containsDragable = true;
+                    // v.setBackgroundDrawable(enterShape);
+                    break;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    //v.setBackgroundDrawable(normalShape);
+                    if (dropEventNotHandled(event)) {
+                        dragView.setVisibility(View.VISIBLE);
+                    }
+                    break;
+
+                case DragEvent.ACTION_DRAG_EXITED:
+                    containsDragable = false;
+
+                    // v.setBackgroundDrawable(normalShape);
+                    break;
+                case DragEvent.ACTION_DROP:
+                    if (containsDragable) {
+
+//                        checkMove();
+///////////////////////////////////////////
+
+                        dragView.setVisibility(View.VISIBLE);
+                    }
+
+
+//                case DragEvent.ACTION_DROP:
+//                    //              Log.v("Test", "Entered drop");
+//                    fl2 = (FrameLayout) v.getParent();
+//                    imgv2 = (ImageView) fl2.getChildAt(1);
+//                    Piece square = (Piece) imgv2.getTag();
+//
+//                    if(currentPiece.getPossibleMoves().contains(square.getCurrentSquare())){
+//                        //                  imgv.setImageResource(currentPiece.getRessource());
+//                        Piece destinationPiece = lp[square.getCurrentSquare()];
+//                        lp[square.getCurrentSquare()] = currentPiece;
+//                        lp[currentPiece.getCurrentSquare()] = new Piece();
+//
+//                        if((yourBeingCheckedDumbAss(currentPiece))
+//                                ||((destinationPiece instanceof King)
+//                                &&(destinationPiece.getColor() != currentPiece.getColor()))
+//                                ||(whiteTurn != currentPiece.getColor())){
+//                            imgvcp.setImageResource(currentPiece.getRessource());
+//                            imgvcp.setVisibility(View.VISIBLE);
+//                            lp[square.getCurrentSquare()] = destinationPiece;
+//                            lp[currentPiece.getCurrentSquare()] = currentPiece;
+//                        }else{
+//                            imgvcp.setImageResource(currentPiece.getRessource());
+//                            ((ChessActivity) mContext).setNewAdapter(lp);
+//                        }
+
+
+//                    // Dropped, reassign View to ViewGroup
+//                    View view = (View) event.getLocalState();
+//                    ViewGroup owner = (ViewGroup) view.getParent();
+//                    owner.removeView(view);
+//                    LinearLayout container = (LinearLayout) v;
+//                    container.addView(view);
+//                    view.setVisibility(View.VISIBLE);
+                    break;
+
+                default:
+                    break;
             }
+            return true;
         }
     }
 
+//    private void checkMove() {
+////
+//        chessActivity.setPosition2(position);
+//        chessActivity.move();
+//        chessActivity.setHasOne(false);
+//
+//
+//
+//    }
+
+
+    //    squareImage.setOnTouchListener(new View.OnTouchListener() {
+//        @Override
+//        public boolean onTouch(View v, MotionEvent event) {
+//            if(event.getAction() == MotionEvent.ACTION_DOWN) {
+//                if (chessActivity.isHasOne()) {
+//                    chessActivity.setPosition2(position);
+//                    chessActivity.move();
+//                    chessActivity.setHasOne(false);
+//                } else {
+//                    chessActivity.setPosition1(position);
+//                    chessActivity.setHasOne(true);
+//                    squareImage.setBackgroundResource(R.drawable.red_square);
+//                }
+//            }
+//            return true;
+//        }
+//    });
+
+
+
+    private boolean dropEventNotHandled(DragEvent dragEvent) {
+        return !dragEvent.getResult();
+    }
 }
+
+
